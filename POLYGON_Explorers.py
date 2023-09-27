a4bfaade7834053390f2c6ad19a89de623b68ade
Flyn San <flynsarmy@gmail.com>
2023-09-27 20:55:04 +1000

    Add Explorers pack

---
+++ POLYGON_Explorers.py
@@ -0,0 +1,84 @@
+import bpy
+import os
+
+def import_characters():
+    # Deselect all
+    bpy.ops.object.select_all(action='DESELECT')
+
+    # Highlight the 'Characters' collection - https://blender.stackexchange.com/a/248563
+    bpy.context.view_layer.active_layer_collection = \
+        bpy.context.view_layer.layer_collection.children['Characters']
+
+    # Find our character FBX's - https://blender.stackexchange.com/a/253543
+    folder = bpy.path.abspath("//SourceFiles\\Characters")
+    fbxs = [f for f in os.listdir(folder) if f.endswith(".fbx")]
+
+    # Import them
+    for fbx in fbxs:
+        bpy.ops.import_scene.fbx(
+            filepath=os.path.join(folder, fbx),
+            use_anim=False,
+            ignore_leaf_bones=True,
+            force_connect_children=True,
+            automatic_bone_orientation=True,
+        )
+
+    collection = bpy.data.collections["Characters"]
+
+    # Loop through Characters
+    first_armature = None
+    for obj in collection.objects:
+        # Save the first armature object
+        if first_armature == None and obj.type == 'ARMATURE':
+            first_armature = obj
+            continue
+
+        if first_armature != None and obj.type == 'MESH':
+            if obj.modifiers and obj.modifiers[0].type == 'ARMATURE':
+                # Set all mesh Armature modifiers to the first armature object
+                obj.modifiers[0].object = first_armature
+            # Make our first armature the object parent
+            obj.parent = first_armature
+
+    # Deselect all
+    bpy.ops.object.select_all(action='DESELECT')
+
+    # Select unnecessary armatures
+    first_armature = None
+    for obj in collection.objects:
+        if obj.type == 'ARMATURE':
+            if first_armature == None:
+                first_armature = obj
+            else:
+                obj.select_set(True)
+
+    # Delete them
+    bpy.ops.object.delete()
+
+    # Remove IK bones
+    bpy.context.view_layer.objects.active = first_armature
+    bpy.ops.object.mode_set(mode='EDIT')
+    # Remove the IK bones
+    for bone in first_armature.data.edit_bones:
+        if bone.name.startswith("_ik_"):
+            first_armature.data.edit_bones.remove(bone)
+    bpy.ops.object.mode_set(mode='OBJECT')
+
+    first_armature.name = "Characters"
+
+    # Textures pointing to the wrong directory. Fix that
+    # https://blender.stackexchange.com/a/280804
+    for image in bpy.data.images.values():
+        if image.source == "FILE":
+            filename = os.path.basename(image.filepath)
+            if filename == "PolygonExplorers_Texture_01.psd":
+                image.filepath = bpy.path.abspath(
+                    "//SourceFiles\\Textures\\PolygonExplorers_Texture_01_A.png"
+                )
+            else:
+                # Make absolute
+                image.filepath = bpy.path.abspath(
+                    "//SourceFiles\\Textures\\" + filename
+                )
+
+import_characters()
