573b8ef83c8ffb0160fc62dab35f24b27c33ac7b
Flyn San <flynsarmy@gmail.com>
2023-09-27 12:23:07 +1000

    first commit

---
+++ POLYGON_CityCharacters.py
@@ -0,0 +1,29 @@
+import bpy
+import os
+
+def import_characters():
+    # Deselect all
+    bpy.ops.object.select_all(action='DESELECT')
+
+    # Highlight the 'Characters' collection - https://blender.stackexchange.com/a/248563
+    bpy.context.view_layer.active_layer_collection = \
+        bpy.context.view_layer.layer_collection.children['Characters']
+
+    # Import characters
+    bpy.ops.import_scene.fbx(
+        filepath=bpy.path.abspath("//Source_Files\\FBX\\Character.fbx"),
+        use_anim=False,
+        ignore_leaf_bones=True,
+        force_connect_children=True,
+        automatic_bone_orientation=True,
+    )
+
+    # Textures pointing to the wrong directory. Fix that
+    for image in bpy.data.images.values():
+        if image.source == "FILE":
+            # Make absolute
+            image.filepath = bpy.path.abspath(
+                "//Source_Files\\Textures\\" + os.path.basename(image.filepath)
+            )
+
+import_characters()
