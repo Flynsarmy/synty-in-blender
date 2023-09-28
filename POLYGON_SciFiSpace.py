1ba802e0c2c87a4b3cab45c0a42143475821c564
Flyn San <flynsarmy@gmail.com>
2023-09-28 12:53:00 +1000

    Add SciFi Space pack

---
+++ POLYGON_SciFiSpace.py
@@ -0,0 +1,109 @@
+import bpy
+import os
+
+def import_characters():
+    # Deselect all
+    bpy.ops.object.select_all(action='DESELECT')
+
+    # Highlight the 'Characters' collection - https://blender.stackexchange.com/a/248563
+    bpy.context.view_layer.active_layer_collection = \
+        bpy.context.view_layer.layer_collection.children['Characters']
+
+    # Find our character FBX's - https://blender.stackexchange.com/a/253543
+    folder = bpy.path.abspath("//_SourceFiles\\Characters")
+    fbxs = [f for f in os.listdir(folder) if \
+        f.endswith(".fbx") \
+        and not "_Suit_" in f \
+        and not "_Head_" in f \
+        and not f.startswith("SK_Chr_Attach_") \
+        # The SpaceSoldier's not ending in 01.fbx are ASCII. Skip them
+        and not (f.startswith("SK_Chr_SpaceSoldier_") and not f.endswith("01.fbx"))
+    ]
+
+    # Import them
+    for fbx in fbxs:
+        bpy.ops.import_scene.fbx(
+            filepath=os.path.join(folder, fbx),
+            use_anim=False,
+            ignore_leaf_bones=True,
+            force_connect_children=True,
+            automatic_bone_orientation=True,
+        )
+
+    collection = bpy.data.collections["Characters"]
+
+    # Loop through Characters
+    first_armature = None
+    for obj in collection.objects:
+        # Save the first armature object
+        if first_armature == None and obj.type == 'ARMATURE':
+            first_armature = obj
+            continue
+
+        if first_armature != None and obj.type == 'MESH':
+            if obj.modifiers and obj.modifiers[0].type == 'ARMATURE':
+                # Set all mesh Armature modifiers to the first armature object
+                obj.modifiers[0].object = first_armature
+
+            # Add missing texture node to the material
+            texture_path = bpy.path.abspath("//_SourceFiles\\Textures\\PolygonSciFiSpace_Texture_01_A.png")
+            for material_slot in obj.material_slots:
+                material = material_slot.material
+
+                # https://blender.stackexchange.com/a/129014
+                bsdf = material.node_tree.nodes["Principled BSDF"]
+                tex_image = material.node_tree.nodes.new('ShaderNodeTexImage')
+                tex_image.image = bpy.data.images.load(texture_path)
+                material.node_tree.links.new(bsdf.inputs['Base Color'], tex_image.outputs['Color'])
+
+            # Make our first armature the object parent
+            obj.parent = first_armature
+
+    # Deselect all
+    bpy.ops.object.select_all(action='DESELECT')
+
+    # Remove IK bones
+    bpy.context.view_layer.objects.active = first_armature
+    bpy.ops.object.mode_set(mode='EDIT')
+    # Remove the IK bones
+    for bone in first_armature.data.edit_bones:
+        if bone.name.startswith("ik_"):
+            first_armature.data.edit_bones.remove(bone)
+    bpy.ops.object.mode_set(mode='OBJECT')
+
+    # Select unnecessary armatures
+    first_armature = None
+    for obj in collection.objects:
+        if obj.type == 'ARMATURE':
+            if first_armature == None:
+                first_armature = obj
+            else:
+                obj.select_set(True)
+
+    # Delete them
+    bpy.ops.object.delete()
+
+    # Apply location, rotation, scale to deltas
+    for obj in collection.all_objects:
+        obj.delta_location += obj.location
+        obj.location = (0, 0, 0)
+        obj.delta_rotation_euler.rotate(obj.rotation_euler)
+        obj.rotation_euler = (0, 0, 0)
+        obj.delta_scale = obj.scale
+        obj.scale = (1, 1, 1)
+
+    first_armature.name = "Characters"
+
+    # Textures pointing to the wrong directory. Fix that
+    # https://blender.stackexchange.com/a/280804
+    for image in bpy.data.images.values():
+        if image.source == "FILE":
+            # Textures are pointing towards PolygonCity. Fix that
+            filename = os.path.basename(image.filepath)
+
+            # Make absolute
+            image.filepath = bpy.path.abspath(
+                "//_SourceFiles\\Textures\\" + filename
+            )
+
+import_characters()
